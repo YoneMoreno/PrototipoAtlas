95b05d9648e36b59047f204cf3aa6c0b0c298717
YonePC <YonePC@YONE>
2018-01-30 18:07:17 +0000

    add show image after loading it. TODO unit test for filereader and image

--- test/readImageSpec.js
+++ test/readImageSpec.js
@@ -2,16 +2,39 @@ function getDOM(){
     var fileUpload = document.getElementById('fileUpload');
     var canvas  = document.getElementById('canvas');
     var ctx = canvas.getContext("2d");
-    console.log(ctx);
     return [fileUpload, canvas, ctx]
 }
-getDOM();
+DOM = getDOM();
+fileUpload = DOM[0];
+canvas = DOM[1];
+ctx = DOM[2];
+
+function readImage() {
+    if ( this.files && this.files[0] ) {
+        var FR= new FileReader();
+        var img = new Image();
+        FR.onload = function(e) {
+            img.src = e.target.result;
+            img.onload = function() {
+                ctx.drawImage(img, 0, 0, 512, 512);
+            };
+        };
+        FR.readAsDataURL( this.files[0] );
+    }
+    return [this.files, FR, img, ctx];
+}
+
+fileUpload.onchange = readImage;
+
 
 describe('readImage', function () {
-    it('should have fileUpload, canvas and context ', function () {
+    it('should get context ', function () {
         DOM = getDOM();
         ctx = DOM[2];
         expect(ctx).not.toBeNull();
+    })
+    it('should get at least one file ', function () {
+
     })
 
 });
