03629ed54561202d4f6f37d006dbcc285cd977bd
YonePC <YonePC@YONE>
2018-01-30 16:37:33 +0000

    Add canvas

---
+++ test/canvasSpec.js
@@ -0,0 +1,20 @@
+function createCanvas() {
+    body = document.getElementsByTagName("BODY")[0];
+    canvas = document.createElement("canvas");
+    canvas.setAttribute("width", "512");
+    canvas.setAttribute("height", "512");
+    body.appendChild(canvas);
+    return canvas;
+}
+
+createCanvas();
+
+describe('canvasSpec', function () {
+    it('should have a canvas with width and height ', function () {
+        testCanvas = createCanvas();
+        expect(testCanvas.nodeName).toEqual("CANVAS");
+        expect(testCanvas.getAttribute("width")).toEqual("512");
+        expect(testCanvas.getAttribute("height")).toEqual("512");
+    })
+
+});
--- test/fileUploadSpec.js
+++ test/fileUploadSpec.js
@@ -6,13 +6,16 @@ function createUploadInput() {
     body.appendChild(upload);
     return upload;
 }
+
 createUploadInput();
 
 describe('fileUpload', function () {
     it('should have a file upload input, with type  and id. ', function () {
-        expect(createUploadInput().nodeName).toEqual("INPUT");
-        expect(createUploadInput().getAttribute("type")).toEqual("file");
-        expect(createUploadInput().getAttribute("id")).toEqual("fileUpload");
+        uploadInput = createUploadInput();
+
+        expect(uploadInput.nodeName).toEqual("INPUT");
+        expect(uploadInput.getAttribute("type")).toEqual("file");
+        expect(uploadInput.getAttribute("id")).toEqual("fileUpload");
     })
 
 });
