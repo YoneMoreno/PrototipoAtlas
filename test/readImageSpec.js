cf2ca6c563ecad58549dac45f6facd457d8830b5
YonePC <YonePC@YONE>
2018-02-01 12:33:00 +0000

    refactor: extract fileReadOnLoad

--- test/readImageSpec.js
+++ test/readImageSpec.js
@@ -9,16 +9,20 @@ fileUpload = DOM[0];
 canvas = DOM[1];
 ctx = DOM[2];
 
+function fileReaderOnLoad(img) {
+    return function (e) {
+        img.src = e.target.result;
+        img.onload = function () {
+            ctx.drawImage(img, 0, 0, 512, 512);
+        };
+    };
+}
+
 function readImage() {
     if ( this.files && this.files[0] ) {
         var FR= getFileReader();
         var img = getImage();
-        FR.onload = function(e) {
-            img.src = e.target.result;
-            img.onload = function() {
-                ctx.drawImage(img, 0, 0, 512, 512);
-            };
-        };
+        FR.onload = fileReaderOnLoad(img);
         FR.readAsDataURL( this.files[0] );
     }
     return [this.files, FR, img, ctx];
